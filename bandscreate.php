7274a6a3ea515ed042ec5315f24f007d90362286
ThomasKiwi18 <2176924@talnet.nl>
2023-12-13 14:06:38 +0100

    Updated PHP FILE
    yessir

--- bandscreate.php
+++ bandscreate.php
@@ -1,4 +1,5 @@
 <form action= '/fullstack/bandscreate.php' method= "post">
+<!--tabel bands --->
 <input name ='name' type='text'> 
 <input type = 'submit'>
 <input type="checkbox" id="genres1" name="genre[]" value="Pop">
@@ -6,11 +7,10 @@
 <input type="checkbox" id="genres3" name="genre[]" value="Hip-Hop">
 <input type="checkbox" id="genres4" name="genre[]" value="Metal">
 <input type="date" id="datum" name="date" value="date">
+<input type="time" id="tijd" name="time" value="time">
+
 
 </form>
-<script>
-alert("Hello! I am an alert box!!");
-    </script>
 
 <?php
 include('database.php');
@@ -22,14 +22,15 @@ if(isset($_POST['genre'])) {
     if (isset($_POST['name'])) {
         $name = $_POST['name'];
         $date = $_POST['date'];
+        $time = $_POST['time'];
 
-        if ($name != '') {
+        if ($name != '' && $date != '' && $time != '') {
             $genre = implode(',', $_POST['genre']);
             
-            $sql = $conn->prepare("INSERT INTO bands (BandNaam, Genre, Datum) VALUES (?, ?,?)");
+            $sql = $conn->prepare("INSERT INTO bands (BandNaam, Genre, Datum, Tijd) VALUES (?, ?, ?, ?)");
             
             if ($sql) {
-                $sql->bind_param("sss", $name, $genre, $date);
+                $sql->bind_param("ssss", $name, $genre, $date, $time);
                 $sql->execute();
                 $sql->close(); 
                 echo "Data inserted successfully!";
@@ -37,11 +38,11 @@ if(isset($_POST['genre'])) {
                 echo "Error: " . $conn->error;
             }
         } else {
-            echo "Geen naam toegevoegd";
+            echo "Informatie niet toegevoegd!";
         }
     } 
 } else {
-    echo "Geen genre toegevoegd";
+    echo "Informatie niet toegevoegd! ";
 } 
 
 ?>
