22acabfae1f678fde164096a61e61f95abcaa305
ThomasKiwi18 <2176924@talnet.nl>
2023-12-18 14:09:47 +0100

    working php files

--- event.php
+++ event.php
@@ -1,15 +1,37 @@
-<form action= '/fullstack/event.php' method= "post">
-<input type="date" id="datum" name="datum" value="datum">Datum van optreden</input>
-<input type="time" id="tijd" name="tijd" value="tijd">Tijd van optreden</input>
-<input type="text" id="eventnaam" name="eventnaam">Naam van het event</input>
-<input type="number" min="1" step="any" id="entreeprijs" name="entreeprijs" value="entreeprijs">Eentreeprijs</input>
-<input type = 'submit'>
+<form action="/fullstack/event.php" method="post">
+    <label for="datum">Datum van optreden</label>
+    <input type="date" id="datum" name="datum">
+    
+    <label for="tijd">Tijd van optreden</label>
+    <input type="time" id="tijd" name="tijd">
+    
+    <label for="eventnaam">Naam van het event</label>
+    <input type="text" id="eventnaam" name="eventnaam">
+    
+    <label for="entreeprijs">Entreeprijs</label>
+    <input type="number" min="1" step="any" id="entreeprijs" name="entreeprijs">
+    
+    <input type="submit">
 </form>
 
 
 
 <?php
 include('database.php');
+
+$sqlbands = "SELECT idbands, BandNaam, Genre FROM bands";
+
+$result = $conn->query($sqlbands);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["idbands"]. " - BandNaam: " . $row["BandNaam"]. " - Genre(s): " . $row["Genre"]. "<br>";
+    }
+  } else {
+    echo "0 bands! ";
+  }
+ 
+
 $date = isset($_POST['datum']) ? $_POST['datum'] : '';
 $time = isset($_POST['tijd']) ? $_POST['tijd'] : '';
 $evnaam = isset($_POST['eventnaam']) ? $_POST['eventnaam'] : '';
@@ -21,12 +43,15 @@ if($time != '' && $date != '' && $evnaam != '' && $enprijs != ''){
     if ($sqlevent) {
         $sqlevent->bind_param("ssss", $date, $time, $evnaam, $enprijs);
         $sqlevent->execute();
-        $sqlevent->close(); 
-        echo "Event Added! ";
-    } else {
-        echo "Error: " . $conn->error;
-    }
-}else {
+        $sqlevent->close();
+        echo 'event added';
+    }else {
+            echo "Error: " . $conn->error;
+        }
+        
+    
+    }else {
     echo "Data niet toegevoegd!";
-} 
+}
+
 ?>
